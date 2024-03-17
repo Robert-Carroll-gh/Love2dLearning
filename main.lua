19d01c5cf9eca45b0cc4208b4a0d3c944f4c4753
Robert Carroll <robert.carroll.eportfolio@gmail.com>
2024-03-16 23:06:55 -0600

    basic player movement

--- main.lua
+++ main.lua
@@ -1,4 +1,28 @@
-print("Hello World!")
 
+player = {
+	x = 400,
+	y = 10,
+	color = {255,0,0},
+	size = 50,
+}
 
-print("this is a new feature")
+love.update = function()
+	if love.keyboard.isDown("down") then
+		player.y = player.y + 5
+	end
+	if love.keyboard.isDown("up") then
+		player.y = player.y - 5
+	end
+	if love.keyboard.isDown("left") then
+		player.x = player.x - 5
+	end
+	if love.keyboard.isDown("right") then
+		player.x = player.x + 5
+	end
+end
+
+love.draw = function()
+	love.graphics.setColor(player.color[1], player.color[2], player.color[3])
+	
+	love.graphics.circle("fill", player.x, player.y, player.size)
+end
