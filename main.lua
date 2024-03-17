5d9e6a173edf63d4bd34a363820d990cad71954b
Derago <deragoduong@gmail.com>
2024-03-16 23:53:14 -0600

    Circle Attempt 1

--- main.lua
+++ main.lua
@@ -1,4 +1,11 @@
-print("Hello World!")
+love.graphics.circle( "fill", 300, 100, 50 )
 
+function love.load()
+   Player = circle
+   x = 50
+   y = 50
+end
 
-print("this is a new feature")
+function love.draw()
+   love.graphics.draw(circle, x, y)
+end
