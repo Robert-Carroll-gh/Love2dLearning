20f2b8ee226b4ba9d114d9a8280f45ea0d8bcaa2
Derago <deragoduong@gmail.com>
2024-03-17 01:01:25 -0600

    Update 3.5

--- main.lua
+++ main.lua
@@ -4,15 +4,18 @@ love.draw = function()
 end
 
 love.keypressed = function()
-	down = love.keypressed.isDown( joystick, button )
+end
 
 player = {
 	x = 400,
 	y = 300,
 	color = { 0, 255, 255},
 	size = 25,
+	speedY = 0,
+	jumpSpeed
 }
 
+--[[
 player.jump = function()
 	if love.keypressed.isDown('w') then
         if player.jump then
@@ -21,6 +24,7 @@ player.jump = function()
     	end
     end
 end
+]]--
 
 love.draw = function()
 	love.graphics.setColor( player.color[1],  player.color[2], player.color[3] )
@@ -28,6 +32,10 @@ love.draw = function()
 end
 
 love.update = function()
+
+	if player.y < 405 then
+		player.y = player.y + player.speedY
+	end
 	if love.keyboard.isDown("w") then
 		player.y = player.y - 5
 	end
@@ -37,8 +45,4 @@ love.update = function()
 	if love.keyboard.isDown("d") then
 		player.x = player.x + 5
 	end
-end
-
-love.keypressed.isDown = function("w")
-   player.jumping()
 end
