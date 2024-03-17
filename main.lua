a9ee87bc48c6add0f4aaea0208acafc055a8d051
Derago <deragoduong@gmail.com>
2024-03-17 00:32:34 -0600

    Update 2

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 love.draw = function()
 	love.graphics.setColor( 0, 255, 255 )
 	love.graphics.circle( "fill", 400, 300, 25 )
@@ -5,3 +6,42 @@ end
 
 love.joystick = function()
 	down = love.joystick.isDown( joystick, button )
+=======
+player = {
+	x = 400,
+	y = 300,
+	color = { 0, 255, 255},
+	size = 25,
+}
+
+player.jump = function()
+	if joyStick:isGamepadDown('a') then
+        if player.jump then
+	       player.speedY = player.jumpSpeed
+            player.jump = false
+    	end
+    end
+end
+
+love.draw = function()
+	love.graphics.setColor( player.color[1],  player.color[2], player.color[3] )
+	love.graphics.circle( "fill", player.x, player.y, player.size )
+end
+
+love.update = function()
+	if love.keyboard.isDown("w") then
+		player.y = player.y - 5
+	end
+	if love.keyboard.isDown("a") then
+		player.x = player.x - 5
+	end
+	if love.keyboard.isDown("d") then
+		player.x = player.x + 5
+	end
+end
+
+love.joystickpressed = function("a")
+   player:jumping()
+end
+
+>>>>>>> Stashed changes
